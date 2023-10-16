81df7d5794dcf66bcbb51cb7c22d87d434c1d62b
abubkar <abubakaraw8@gmail.com>
2023-10-16 15:42:33 +0500

    project completed but todo item not working debuging remain

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import {TodoProvider} from './contexts'
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
     const [todos , setTodos] = useState([])
@@ -18,7 +20,7 @@ function App() {
     }
 
     const toggleComplete = (id) => {
-        setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
     }
 
     useEffect(() => {
@@ -39,9 +41,15 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => {
+                            <div key={todo.id} className='w-full'>
+                                <TodoItem todo={todo}/>
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
